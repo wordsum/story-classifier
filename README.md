8fcf7fbfd3a72ea1bbc2ae51adcba1dd515c2d3a
Kalab J. Oster <kalabjoster@wordprostitute.com>
2025-09-23 21:04:40 -0700

    Write feature to create training and test data.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-A tool to classify a story into its genre.
+A tool to classify a story into the genre of fantasy, science fiction, contemporary.
+
+Example Command story classifiction (WIP): `poetry run python3 main.py --story "PATH/FILE.txt"`
+
+Example Command create training data and test data: `poetry run python3 main.py --prepare "PATH/FILE.txt" --classification science_fiction --output training.csv`
--- src/main.py
+++ src/main.py
@@ -1,10 +1,8 @@
 import argparse
 import spacy
+import csv
 
 def input_file(story):
-	"""
-	Reads a file and remove new lines and empty lines.
-	"""
 	words = []
 	try:
 		with open(story, 'r') as input:
@@ -18,28 +16,49 @@ def input_file(story):
 		return f"Error: File not found: {story}"
 	except Exception as e:
 		return f"And error happened: {e}"
+	
+def output_file(words, classification, file):
+	try:
+		with open(file, 'a', newline='') as csvfile:
+			csv_writer = csv.writer(csvfile)
+			csv_writer.writerows([[classification, words]])
+	except FileNotFoundError:
+		return f"Error: File not found to write words."
+	except Exception as e:
+		return f"And error happened: {e}"	
+
 
 def prepare_words(words):
 	nlp = spacy.load("en_core_web_sm")
-	words = nlp(words.lower())
-	words_not_stop = [token.text for token in words if not token.is_punct and not token.is_stop]
 
+	words_lower = nlp(words.lower())
+	words_stop_punc = [token.lemma_ for token in words_lower if not token.is_punct and not token.is_stop]
 
-	return words_not_stop
+	return " ".join(words_stop_punc)
 
 def classify(story):
 	words = input_file(story)
 	words_prep = prepare_words(words)
 	
-
 	print(words_prep)
 	return words
 
 def main():
-	parser = argparse.ArgumentParser(description="Classify a story.")
-	parser.add_argument("--story", type=str, help="Path to the file with the story.")
+	parser = argparse.ArgumentParser(description="Story Classifier.")
+	parser.add_argument("--prepare", type=str, help="Directory and file to clean.")
+	parser.add_argument("--classification", type=str, help="The classification of the prepared file.")
+	parser.add_argument("--output", type=str, help="Directory and csv file to output prepared.")
+	parser.add_argument("--story", type=str, help="Directory path and file to the story.")
 	args = vars(parser.parse_args())
-	classify(args['story'])
+    
+	if args['prepare'] is not None and args['classification'] and args['output']:
+		words = input_file(args['prepare'])
+		prepared_words = prepare_words(words)
+		output_file(prepared_words, args['classification'], args['output'])
+
+     
+	if args['story'] is not None:
+		classify(args['story'])
 
 if __name__ == "__main__":
 	main()
